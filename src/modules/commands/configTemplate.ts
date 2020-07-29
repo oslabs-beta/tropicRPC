97834e6949b70af32495964353c1a4aab46c5454
Steve Canavan <stevenrosscanavan@gmail.com>
2020-07-29 12:40:11 -0400

    added functionality for configuration with IP server addresses

--- src/modules/commands/configTemplate.ts
+++ src/modules/commands/configTemplate.ts
@@ -6,6 +6,8 @@ const config = {
   // change 3000 to the port number on which your server runs
   portNumber: 3000,
 
+  // populate '' with the IP address of your server
+  ipAddress: '',
   // change "./demo.proto" to the relative path from the root directory to your proto file
   protoFile: 'src/proto/demo.proto',
 
